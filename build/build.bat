f61eb18803948524cbc4492407594ac9635a3a31
Austin Dolan <adolan@ncsu.edu>
2025-02-15 14:27:57 -0500

    build scripts

--- build/build.bat
+++ build/build.bat
@@ -1,4 +1,27 @@
 @echo off
 
-cmake ..
+:: Check if argument is provided
+if "%1"=="" (
+    echo Usage: build.bat [make|ninja|vs]
+    exit /b 1
+)
+
+:: Select generator
+if "%1"=="make" (
+    set GENERATOR="MinGW Makefiles"
+) else if "%1"=="ninja" (
+    set GENERATOR="Ninja"
+) else if "%1"=="vs" (
+    set GENERATOR="Visual Studio 17 2022"
+) else (
+    echo Invalid system. Use 'make', 'ninja', or 'vs'.
+    exit /b 1
+)
+
+
+:: Run CMake and build
+cmake .. -G %GENERATOR%
 cmake --build .
+
+:: Return to project root
+cd ..
--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,31 @@
 #!/bin/bash
-set -e
-
-# Default to Debug if no argument is provided
-BUILD_TYPE=${1:-Debug}
 
 
-cmake .. -DCMAKE_BUILD_TYPE=$BUILD_TYPE
+# Check for argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 [make|ninja|vs]"
+    exit 1
+fi
+
+# Select the appropriate generator
+case "$1" in
+    make)
+        GENERATOR="Unix Makefiles"
+        ;;
+    ninja)
+        GENERATOR="Ninja"
+        ;;
+    vs)
+        GENERATOR="Visual Studio 17 2022"
+        ;;
+    *)
+        echo "Invalid system. Use 'make', 'ninja', or 'vs'."
+        exit 1
+        ;;
+esac
+
+
+# Run CMake and build
+cmake .. -G "$GENERATOR"
 cmake --build .
+
